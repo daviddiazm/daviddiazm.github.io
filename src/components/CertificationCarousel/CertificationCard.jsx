ed46f04667a44ece5755f4f9ad69a0d7e0ca3f50
david <davidcol123dm@gmail.com>
2024-12-22 19:22:18 -0500

    mano movil en mobile

--- src/components/CertificationCarousel/CertificationCard.jsx
+++ src/components/CertificationCarousel/CertificationCard.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import './CertificationCard.css'
 
-const CertificationCard = ({ certification , moveNext, movePrev }) => {
+const CertificationCard = ({ certification, moveNext, movePrev }) => {
+  const { certificationName, certificationDescription, certificationImg } = certification
+
   const [startX, setStartX] = useState(0);
+  const [isVisible, setIsVisible] = useState(true);
 
   const handleTouchStart = (e) => {
     setStartX(e.touches[0].clientX);
@@ -13,8 +16,8 @@ const CertificationCard = ({ certification , moveNext, movePrev }) => {
     const endX = e.changedTouches[0].clientX;
     const diff = startX - endX;
 
-    console.log({endX, diff});
-    
+    console.log({ endX, diff });
+
 
     if (diff > 50) {
       moveNext();
@@ -22,21 +25,33 @@ const CertificationCard = ({ certification , moveNext, movePrev }) => {
       movePrev();
     }
   };
-  
 
-  const {certificationName, certificationDescription, certificationImg} = certification
+  useEffect(() => {
+    // Temporizador para ocultar la imagen después de un tiempo
+    const timer = setTimeout(() => {
+      setIsVisible(!isVisible);
+    }, 3000); // La imagen desaparecerá después de 3 segundos
+
+    // Limpiar el temporizador si el componente se desmonta
+    return () => clearTimeout(timer);
+  }, [isVisible]);
+
   return (
-    <div 
+    <div
       className='card-certification'
       onTouchStart={handleTouchStart}
       onTouchEnd={handleTouchEnd}>
 
+      {isVisible && (
+        <img src={'/imgs/png_img/mano.png'} alt="dedo hacia la izquierda" className="animated-image" />
+      )}
+
       <div className='landing__1'>
-        {/* <img src="/imgs/png_img/Frame 3532.png" alt="" class="back__landing"/> */}
-        <img src="/imgs/png_img/Frame 3530.png" alt="" class="back__landing"/>
+        {/* <img src="/imgs/png_img/Frame 3532.png" alt="" className="back__landing"/> */}
+        <img src="/imgs/png_img/Frame 3530.png" alt="" className="back__landing" />
         <img src={`${certificationImg}`} alt="" className='img_landin' />
       </div>
-      
+
       <article className='certification_text'>
         <h3> {certificationName} </h3>
         <p> {certificationDescription} </p>
