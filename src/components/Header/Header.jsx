0a970e933d84d8568833ce52aaa9c60da07c9a56
David <davidcol123dm@gmail.com.com>
2026-09-03 11:01:01 -0500

    cambio header para trabajo de la U

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,12 +14,13 @@ const Header = () => {
         <article className="header__1">
           <h2><span>Hola!</span> Soy Desarrollador de software.</h2>
           <h3>David Diaz Montenegro</h3>
-          <p>Desarrollador de software con experiencia en proyectos de desarrollo web, especializado en front-end y con habilidades
-            comprobadas en tecnologías como React , Vue , y Angular . En el back-end , cuento con experiencia utilizando Node.js, PHP y Python
-            junto con la biblioteca FastAPI . Poseo sólidos fundamentos en bases de datos relacionales como PostgreSQL , MySQL , y Oracle SQL .
-            Además, aplico principios de UX con el objetivo de contribuir a que la web sea un espacio más accesible y equitativo para todos.</p>
+          <p>
+            Futuro Ingeniero Informático enfocado en proyectarse como Desarrollador Full Stack. Me preparo para desempeñarme en el campo de la creación y arquitectura de aplicaciones web integrales. En mi futuro ejercicio profesional, asumiré el desarrollo de soluciones de software desde cero, gestionando tanto la experiencia visual del usuario (frontend) como la lógica de servidores y bases de datos (backend). Mi trabajo abarcará desde la implementación de interfaces interactivas apoyadas en herramientas como React o Angular y diseño UX, hasta la construcción de servicios robustos en Java con Spring Boot o Python.  En su ruta de crecimiento, David está fortaleciendo actualmente su competencia en el idioma inglés y proyecta complementar su carrera universitaria con certificaciones profesionales en desarrollo Full Stack, servicios en la nube (AWS) y UX.  El valor distintivo que aportaré a las organizaciones radica en su visión práctica del software: busca conectar la tecnología con la optimización de procesos de producción reales, combinando el rigor técnico con habilidades blandas fundamentales como la comunicación asertiva y la receptividad para el trabajo en equipo.  Lo que hace altamente interesante su perfil es esa combinación entre sensibilidad por el diseño de usuario, solidez backend y la motivación clara de aplicar la informática para transformar ideas en productos digitales tangibles. Representé el inicio del camino de un ingeniero que construirá soluciones tecnológicas con impacto real en la industria.
+          </p>
+          
+          
           <Button text={"Descargar CV"} onClick={downloadCV} />
-          {/* <a href="/pdfs/David Diaz Montengro CV.pdf" download="David Diaz Montengro CV.pdf" >aaa</a> */}
+          
         </article>
         <section className="header__2">
           <img src="/imgs/AREmoji_20230204_193331_24458.gif" alt="" />
